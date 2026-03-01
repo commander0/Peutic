8b5eecb82709a139f28d517c19c38899efbfbbbc
commander0 <Djbauer1993@gmail.com>
2026-02-28 22:06:23 -0800

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -140,7 +140,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ onLoginClick }) => {
                 </div>
             )}
 
-            <div className="fixed inset-0 pointer-events-none overflow-hidden z-0">
+            <div className="fixed inset-0 pointer-events-none overflow-hidden z-0 bg-[#FFFBEB] dark:bg-[#0A0A0A]">
                 <video
                     autoPlay
                     loop
