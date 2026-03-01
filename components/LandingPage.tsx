df55cbba7266d170266965b76f8dce2ac2ab6d21
commander0 <Djbauer1993@gmail.com>
2026-03-01 14:36:37 -0800

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -152,9 +152,9 @@ const LandingPage: React.FC<LandingPageProps> = ({ onLoginClick }) => {
                     >
                         <source src="https://videos.pexels.com/video-files/3249935/3249935-hd_1920_1080_25fps.mp4" type="video/mp4" />
                     </video>
-                    {/* Golden tints for both light and dark mode */}
-                    <div className="absolute inset-0 bg-[#facc15]/30 mix-blend-overlay pointer-events-none dark:bg-yellow-500/10 dark:mix-blend-color-dodge"></div>
-                    <div className="absolute inset-0 bg-[#fde9a0]/40 mix-blend-multiply pointer-events-none dark:bg-[#1C1F26]/30"></div>
+                    {/* Golden tints globally applied to both light and dark modes */}
+                    <div className="absolute inset-0 bg-[#facc15]/30 mix-blend-overlay pointer-events-none"></div>
+                    <div className="absolute inset-0 bg-[#fde9a0]/40 mix-blend-multiply pointer-events-none"></div>
 
                     <div className="absolute inset-x-0 bottom-0 h-[40vh] bg-gradient-to-t from-[#fde9a0] via-[#fde9a0]/60 to-transparent dark:from-[#1C1F26] dark:via-[#1C1F26]/50"></div>
                 </div>
